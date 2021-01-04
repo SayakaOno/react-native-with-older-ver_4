5f6602b3c295d56b2e2eb53493d27c5ae30dd5cb
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-04 08:10:14 -0800

    implement login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducer';
 import config from '../firebaseConfig';
 import LoginForm from './components/LoginForm';
 import {Header} from './components/common';
 
 const App = () => {
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+  }, []);
+
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
   return (
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <View>
         <Header headerText="App" />
         <LoginForm />
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {connect} from 'react-redux';
-import {Card, CardSection, Input, Button} from './common';
-import {emailChanged, passwordChanged} from '../actions';
+import {Card, CardSection, Input, Button, Spinner} from './common';
+import {emailChanged, passwordChanged, loginUser} from '../actions';
 
-const LoginForm = ({email, password, emailChanged, passwordChanged}) => {
+const LoginForm = ({
+  email,
+  password,
+  emailChanged,
+  passwordChanged,
+  loginUser,
+  error,
+  loading,
+}) => {
   const onEmailChange = text => {
     emailChanged(text);
   };
 
   const onPasswordChange = text => {
     passwordChanged(text);
+  };
+
+  const onButtonPress = () => {
+    loginUser(email, password);
+  };
+
+  const renderButton = () => {
+    if (loading) {
+      return <Spinner size="large" />;
+    }
+
+    return <Button onPress={onButtonPress}>Login</Button>;
+  };
+
+  const renderError = () => {
+    if (error) {
+      return (
+        <View style={{backgroundColor: 'white'}}>
+          <Text style={styles.errorTextStyle}>{error}</Text>
+        </View>
+      );
+    }
   };
 
   return (
@@ -26,6 +56,7 @@ const LoginForm = ({email, password, emailChanged, passwordChanged}) => {
 
       <CardSection>
         <Input
+          secureTextEntry
           label="Password"
           placeholder="password"
           onChangeText={onPasswordChange}
@@ -33,19 +64,29 @@ const LoginForm = ({email, password, emailChanged, passwordChanged}) => {
         />
       </CardSection>
 
-      <CardSection>
-        <Button>Login</Button>
-      </CardSection>
+      {renderError()}
+
+      <CardSection>{renderButton()}</CardSection>
     </Card>
   );
 };
 
+const styles = StyleSheet.create({
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+});
+
 const mapStateToProps = state => ({
   email: state.auth.email,
   password: state.auth.password,
+  error: state.auth.error,
+  loading: state.auth.loading,
 });
 
 export default connect(
   mapStateToProps,
-  {emailChanged, passwordChanged},
+  {emailChanged, passwordChanged, loginUser},
 )(LoginForm);
