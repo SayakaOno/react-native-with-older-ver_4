84580dc03db3ffb9dfd684a33eb7e14cf088b786
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-06 09:08:29 -0800

    extract EmployeeForm from EmployeeCreate

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, TouchableWithoutFeedback, View} from 'react-native';
 import {CardSection} from './common';
+import {Actions} from 'react-native-router-flux';
 
 const ListItem = ({employee}) => {
+  const onRowPress = () => {
+    Actions.employeeCreate({employee});
+  };
+
   return (
-    <CardSection>
-      <Text style={styles.titleStyle}>{employee.name}</Text>
-    </CardSection>
+    <TouchableWithoutFeedback onPress={onRowPress}>
+      <View>
+        <CardSection>
+          <Text style={styles.titleStyle}>{employee.name}</Text>
+        </CardSection>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
